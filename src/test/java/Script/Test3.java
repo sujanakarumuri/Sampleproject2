7fb101551c4788d199488f9b4fdec3211b35ee33
sujana <sujana@LAPTOP-9PSLU48T>
2022-08-09 10:44:41 +0530

    Hai all

---
+++ src/test/java/Script/Test3.java
@@ -0,0 +1,15 @@
+package Script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	
+		public void tc3()
+		{
+			Reporter.log("Hello Java and Selenium", true);
+		}
+	
+
+}
